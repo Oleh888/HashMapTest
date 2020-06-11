a63a1528c028b9930d15ff850ca201a814474e42
Oleh Kuzmenko <oleg24453@gmail.com>
2020-06-12 00:24:39 +0300

    changed implementation to the hashmap with open addressing

--- src/main/java/hash/map/impl/MyHashMap.java
+++ src/main/java/hash/map/impl/MyHashMap.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class MyHashMap implements MyMap<Integer, Long> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final double DEFAULT_LOAD_FACTOR = 0.75f;
-    private Node<Integer, Long>[] bucket;
+    private Node[] bucket;
     private int size;
 
     public MyHashMap() {
@@ -17,30 +17,35 @@ public class MyHashMap implements MyMap<Integer, Long> {
         if ((double) size / bucket.length >= DEFAULT_LOAD_FACTOR) {
             resize();
         }
-        Node<Integer, Long> newNode = new Node<>(key, value);
+        Node newNode = new Node(key, value);
         int index = hash(key);
         if (bucket[index] == null) {
             bucket[index] = newNode;
             size++;
+        } else if (Objects.equals(bucket[index].key, key) || bucket[index].key == null) {
+            bucket[index] = newNode;
         } else {
-            Node<Integer, Long> current = getCurrent(bucket[index], newNode);
-            if (Objects.equals(current.key, key)) {
-                current.value = value;
-            } else {
-                current.next = newNode;
-                size++;
+            while (bucket[index] != null && !(Objects.equals(bucket[index].key, key)
+                    && Objects.equals(bucket[index].value, value))) {
+                index++;
+                if (index > size) {
+                    resize();
+                    index = hash(key);
+                }
             }
+            bucket[index] = newNode;
+            size++;
         }
     }
 
     @Override
     public Long getValue(Integer key) {
-        Node<Integer, Long> current = bucket[hash(key)];
-        while (current != null) {
-            if (Objects.equals(current.key, key)) {
-                return current.value;
+        int index = hash(key);
+        while (bucket[index] != null || index <= size) {
+            if (Objects.equals(bucket[index].key, key)) {
+                return bucket[index].value;
             }
-            current = current.next;
+            index++;
         }
         return null;
     }
@@ -54,36 +59,22 @@ public class MyHashMap implements MyMap<Integer, Long> {
         return (key == null) ? 0 : Math.abs(key.hashCode()) % bucket.length;
     }
 
-    private Node<Integer, Long> getCurrent(
-            Node<Integer, Long> startNode, Node<Integer, Long> newNode) {
-        Node<Integer, Long> current = startNode;
-        while (current.next != null) {
-            if (Objects.equals(current.key, newNode.key)) {
-                return current;
-            }
-            current = current.next;
-        }
-        return current;
-    }
-
     private void resize() {
-        Node<Integer, Long>[] oldBucket = bucket;
-        bucket = new Node[bucket.length * 2];
+        Node[] oldBucket = bucket;
+        bucket = new Node[bucket.length * 2];;
         size = 0;
-        for (Node<Integer, Long> element : oldBucket) {
-            while (element != null) {
+        for (Node element : oldBucket) {
+            if (element != null) {
                 put(element.key, element.value);
-                element = element.next;
             }
         }
     }
 
-    private static class Node<K, V> {
-        private final K key;
-        private V value;
-        private Node<K, V> next;
+    private static class Node {
+        private Integer key;
+        private Long value;
 
-        Node(K key, V value) {
+        Node(Integer key, Long value) {
             this.key = key;
             this.value = value;
         }
